da98416773861d27499b5359fa424ef5698ba97c
shuwen yang <yangshuwen7560@gmail.com>
2024-02-21 21:44:48 +0100

    meet some problems in dice_game method, it has too much codes and a bit of messy. Will arrange it

--- src/game/game.py
+++ src/game/game.py
@@ -2,50 +2,63 @@ from dice import Dice
 
 
 class Game:
-    def __init__(self, player1, player2, mode, target_score=100):
-        self.player1 = player1
-        self.player2 = player2
+    def __init__(self, player1, player2, computer, mode, target_score=100):
+        self.players = [player1, player2]
+        self.computer = computer
         self.mode = mode
         self.target_score = target_score
+        self.current_player_index = 0
         self.dice = Dice()
 
     def play_mode(self):
         # a way to switch between two modes
-        if self.mode == 'PvC':
-            self.play_against_computer()
-        elif self.mode == 'PvP':
-            self.play_against_player()
-        else:
-            print("Invalid game mode.")
+        while not self.game_over:
+            if self.mode == 'PvC':
+                self.dice_game()
+                if not self.game_over:
+                    self.computer_turn()
+            elif self.mode == 'PvP':
+                print(f"It is {self.players[self.current_player_index].name}'s turn")
+                self.dice_game()
 
-    def play_against_computer(self):
+    def end_game(self):
+        self.game_over = True
+
+    def end_turn(self):
+        self.current_player_index = (self.current_player_index + 1) % 2
+
+    def dice_game(self):
         # need to change it to be suitable for both PvE and PvP
-        print('It is your turn, "roll" or "hold"')
+        current_player = self.players[self.current_player_index]
+        print('"roll", "hold" or "exit"')
         decision = input()
-        if decision == "roll":
+        if decision.lower() == "roll":
             result = self.dice.roll()
             if result == 1:
-                print("You rolled a 1. Your turn is over.")
-                self.player1.reset_round_points()
-                self.player1.current_points_adjust()
-                # self.play_computer_turn()
+                print(f"{self.players[self.current_player_index].name} rolled a 1. Your turn is over.")
+                current_player.reset_round_points()
+                current_player.current_points_adjust()
+                if self.mode == "PvP":
+                    self.end_turn()
             else:
-                self.player1.add_round_points(result)
-                curr_points = self.player1.current_points(result)
+                current_player.add_round_points(result)
+                curr_points = current_player.current_points(result)
                 print(f'You got {result}. Your total points: {curr_points}')
                 if curr_points >= self.target_score:
-                    print("Congratulations! You win!")
-                else:
-                    self.play_against_computer(curr_points)
-        elif decision == "hold":
-            curr_points = self.player1.current_points(0)
-            self.player1.reset_round_points()
-            print("You decided to hold.")
-            print(f"Your total points: {curr_points}")
-            # self.play_computer_turn()
+                    print(f"Congratulations! {self.players[self.current_player_index].name} win!")
+                    self.end_game()
+        elif decision.lower() == "hold":
+            curr_points = current_player.current_points(0)
+            current_player.reset_round_points()
+            print(f"{self.players[self.current_player_index].name} decided to hold.")
+            print(f"{self.players[self.current_player_index].name} total points: {curr_points}")
+            if self.mode == "PvP":
+                self.end_turn()
+        elif decision.lower() == "Exit":
+            print('Feel free to join again! :)')
+            self.end_game()
         else:
-            print("Invalid decision. Please enter 'roll' or 'hold'.")
-            self.play_against_computer()
+            print("Invalid decision. Please enter 'roll', 'hold' or 'exit'.")
 
     def computer_turn(self):
         print("Computer's turn")
