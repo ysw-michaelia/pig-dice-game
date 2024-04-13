8dd4db851b5113931eaaa8d2d95571d96bb857ea
shuwen yang <yangshuwen7560@gmail.com>
2024-04-13 15:57:57 +0200

    added setup() teardown() for test_setting

--- src/game/test_game_settings.py
+++ src/game/test_game_settings.py
@@ -9,15 +9,20 @@ from score import Score
 class testGameSettings(unittest.TestCase):
     """test the class"""
 
+    def setUp(self):
+        self.score = Score()
+        self.game_setting = GameSettings(self.score)
+
+    def tearDown(self):
+        pass
+
     @patch('game.Game.start_game')
     @patch('game_settings.GameSettings.get_player_name')
     @patch('game_settings.GameSettings.get_difficulty')
     @patch('builtins.input', side_effect=['2', '3', 'Player1'])
     def test_against_computer(self, mock_input, mock_get_diff,
                               mock_get_player_name, mock_start_game):
-        score = Score()
-        game_setting = GameSettings(score)
-        game_setting.against_computer()
+        self.game_setting.against_computer()
 
         mock_get_diff.assert_has_calls([call(1), call(2)])
         mock_get_player_name.assert_called_with(1)
@@ -26,46 +31,34 @@ class testGameSettings(unittest.TestCase):
     @patch('game.Game.start_game')
     @patch('game_settings.GameSettings.get_player_name',
            side_effect=['Player1', 'Player2'])
-    # @patch('dice.Dice')
     def test_against_player(self, mock_get_player_name,
                             mock_start_game):
-        score = Score()
-        game_settings = GameSettings(score)
-        game_settings.against_player()
+        self.game_setting.against_player()
 
-        mock_get_player_name.assert_has_calls([call("Player 1"), call("Player 2")])
-        # mock_dice.assert_called()
-        # mock_dice.assert_called_once_with(probability=None)
-        # self.assertTrue(mock.called)
+        mock_get_player_name.assert_has_calls([call("Player 1"),
+                                               call("Player 2")])
         mock_start_game.assert_called_once()
 
     @patch('builtins.input', side_effect=['3'])
     def test_get_difficulty_probability(self, mock_input):
-        score = Score()
-        game_setting = GameSettings(score)
-        difficulty = game_setting.get_difficulty(1)
+        difficulty = self.game_setting.get_difficulty(1)
         self.assertEqual(difficulty, '3')
         mock_input.assert_called_once_with()
 
     @patch('builtins.input', side_effect=['5'])
     def test_get_difficulty_strategy(self, mock_input):
-        game_settings = GameSettings(None)
-        difficulty = game_settings.get_difficulty(2)
+        difficulty = self.game_setting.get_difficulty(2)
         self.assertEqual(difficulty, '5')
         mock_input.assert_called_once_with()
 
     @patch('builtins.input', side_effect=['computer', 'Player1'])
     def test_get_player_name_pvc(self, mock_input):
-        score = Score()
-        game_setting = GameSettings(score)
-        player = game_setting.get_player_name(1)
+        player = self.game_setting.get_player_name(1)
         self.assertEqual(player.name, 'Player1')
 
     @patch('builtins.input', side_effect=['Player2'])
     def test_get_player_name_pvp(self, mock_input):
-        score = Score()
-        game_setting = GameSettings(score)
-        player = game_setting.get_player_name('Player 2')
+        player = self.game_setting.get_player_name('Player 2')
         self.assertEqual(player.name, 'Player2')
 
 
